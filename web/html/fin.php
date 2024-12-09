00c697b11671cf4b331439106e22ccfca1d90041
Cheick6 <cheicksow384@gmail.com>
2024-12-09 13:13:28 +0100

    Add files via upload

---
+++ web/html/fin.php
@@ -0,0 +1,13 @@
+<?php
+
+"<script src=  "." $src1 "." ></script>";
+"<script src=  "." $src2 "." ></script>";
+
+"</body>
+<footer>
+    &copy; 2024 PingMe . Tous droits réservés
+</footer>
+</html>" ;
+
+
+?>
