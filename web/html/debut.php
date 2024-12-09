b5ecdde3b7fc0ee57ab2a9e0d7111e0039d3bcec
Cheick6 <cheicksow384@gmail.com>
2024-12-09 13:12:41 +0100

    Add files via upload

---
+++ web/html/debut.php
@@ -0,0 +1,12 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="fr"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="' . $rel1 . '" href="' . $href1 . '">
+    <link rel="' . $rel2 . '" href="' . $href2 . '">
+</head>
+<body>';
+?>
