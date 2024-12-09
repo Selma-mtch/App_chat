4bd08fcdfe1cbdc33c92772ab1e75e12f65c23db
Cheick6 <cheicksow384@gmail.com>
2024-12-09 13:54:14 +0100

    Update debut.php
    rajout de la variable $titre

--- web/html/debut.php
+++ web/html/debut.php
@@ -4,7 +4,7 @@ echo '<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
+    <title>'.$titre.'</title>
     <link rel="' . $rel1 . '" href="' . $href1 . '">
     <link rel="' . $rel2 . '" href="' . $href2 . '">
 </head>
