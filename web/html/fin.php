d8b1643a51d69df28287ea422eefd1071a80b3e2
Cheick6 <cheicksow384@gmail.com>
2024-12-12 10:55:13 +0100

    Update fin.php
    echo

--- web/html/fin.php
+++ web/html/fin.php
@@ -1,11 +1,11 @@
 <?php
 
-"<script src=  "." $src1 "." ></script>";
-"<script src=  "." $src2 "." ></script>";
+echo "<script src=  "." $src1 "." ></script>";
+echo "<script src=  "." $src2 "." ></script>";
 
-"</body>
+echo "</body>
 <footer>
-    &copy; 2024 PingMe . Tous droits réservés
+    &copy; 2024 PingMe . Tous droits réservés.
 </footer>
 </html>" ;
 
