7c9d1749a37acbb7b3bb2a52f63af6fb456aac41
Dreskine <dkonte95100@gmail.com>
2024-12-12 12:51:04 +0100

    serveur php

---
+++ serveur-websocket-ratchet.php
@@ -0,0 +1,50 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require 'vendor/autoload.php';
+
+class ChatServer implements \Ratchet\MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(\Ratchet\ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        $remoteAddress = $conn->remoteAddress;
+        echo "Nouvelle connexion établie ({$remoteAddress})\n";
+    }
+
+    public function onMessage(\Ratchet\ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(\Ratchet\ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        $remoteAddress = $conn->remoteAddress;
+        echo "Connexion fermée ({$remoteAddress})\n";
+    }
+
+    public function onError(\Ratchet\ConnectionInterface $conn, \Exception $e) {
+        echo "Erreur : " . $e->getMessage() . "\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ChatServer()
+        )
+    ),
+    8080
+);
+
+$server->run();
