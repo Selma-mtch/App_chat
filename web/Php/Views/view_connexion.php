45a340f6aea04f46c524a5b94a0efd22b1c24027
Selma Mataiche <mataiche.selma77@gmail.com>
2024-12-19 13:42:02 +0100

    Update view_connexion.php
    Correction bug  2: (correction return execute)

--- web/Php/Views/view_connexion.php
+++ web/Php/Views/view_connexion.php
@@ -1,20 +1,24 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="../css/connexion.css">
-    <link rel="stylesheet" href="../css/general.css">
-</head>
-<body> 
+<?php 
+require 'creer_cookie.php';
+
+$titre = "Connexion" ;
+
+$rel1 = "stylesheet";
+$href1= "../css/connexion.css";
+
+$rel2 = "stylesheet";
+$href2 = "../css/general.css";
+
+require 'debut.php';
+
+?>
     <h1>PingMe</h1>
     <img src="../img/logo.png" id="logo" alt="Logo de PingMe">
     <h1>Connectez-vous pour commencer !</h1>
     
     <div id="formulaire">
 
-        <form method="GET" action="index.php">
+        <form method="POST" action="index.php">
              
             <div>
                 <label for="addMail"> <img src="../img/enveloppe.png" alt="Icône enveloppe"/> Adresse mail: </label>
@@ -31,22 +35,19 @@
             </div>
         </form>
 
-        <?php if (isset($message) && !empty($message)) { ?>
+        <?php if (isset($message) && !empty($message)): ?>
         <div class="message">
             <?php echo htmlspecialchars($message); ?>
         </div>
-        <?php }; ?>
+        <?php endif ?>
 
     </div>
 
 <p>Pas encore inscrit ? N'attendez plus !</p>
-    <a href="inscription.html">M'inscrire</a>
     <a href="index.php?action=inscription">M'inscrire</a>
 
-    <script src="../js/submit.js"></script>
-
-</body>
-<footer>  
-    &copy; 2024 PingMe. Tous droit réservés.
-</footer>
-</html>
+<?php 
+$src1 = "../js/submit.js";
+$src2 = "";
+require 'fin.php';
+?>
