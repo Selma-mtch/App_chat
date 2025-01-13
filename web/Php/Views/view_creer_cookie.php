eb978454e86a76a333df6abb84282d893be90290
Cheick6 <cheicksow384@gmail.com>
2025-01-13 16:34:08 +0100

    Update view_creer_cookie.php

--- web/Php/Views/view_creer_cookie.php
+++ web/Php/Views/view_creer_cookie.php
@@ -1,1 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Récupérer les valeurs du formulaire
+    $mail = htmlspecialchars($_POST["addMail"]); // Nom du cookie
+    $password = htmlspecialchars($_POST["password"]); // Valeur du cookie
+    $duree = 86400 ; // Durée en secondes
 
+    // Vérification des données
+    if (!empty($mail) && !empty($password) && $duree > 0) {
+        // Créer le cookie
+        setcookie($mail, $password, time() + $duree, "/");
+
+       // echo "Le cookie '$mail' a été créé avec la valeur '$password' pour une durée de $duree secondes.";
+    } 
+    
+    else {
+        //echo "Veuillez fournir des données valides.";
+    }
+} else {
+    //echo "Aucune donnée reçue.";
+}
+?>
