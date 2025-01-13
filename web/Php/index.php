f1905d1d229f90c1b310446f7f3466d004ff5339
mehdisadki <98515764+mehdisadki@users.noreply.github.com>
2025-01-13 02:48:29 +0100

    Update index.php

--- web/Php/index.php
+++ web/Php/index.php
@@ -1,34 +1,32 @@
 <?php
 
-//Pour avoir la fonction e()
-//require_once "Utils/functions.php";
-//Inclusion du modèle
+// Inclusion des dépendances
 require_once "Models/Model.php";
-//Inclusion de la classe Controller
 require_once "Controllers/Controller.php";
 require_once "Controllers/Controller_connexion.php";
 
-//Liste des contrôleurs -- A RENSEIGNER
-$controllers = ["connexion","accueil"];
-//Nom du contrôleur par défaut-- A RENSEIGNER
+// Liste des contrôleurs
+$controllers = ["connexion", "accueil", "deconnexion", "changePseudo", "changemdp", "inscription"]; 
+
+// Nom du contrôleur par défaut
 $controller_default = "connexion";
 
-//On teste si le paramètre controller existe et correspond à un contrôleur de la liste $controllers
+// Vérifie si le paramètre controller existe et correspond à un contrôleur valide
 if (isset($_GET['controller']) and in_array($_GET['controller'], $controllers)) {
     $nom_controller = $_GET['controller'];
 } else {
     $nom_controller = $controller_default;
 }
 
-//On détermine le nom de la classe du contrôleur
+// Détermine le nom de la classe du contrôleur
 $nom_classe = 'Controller_' . $nom_controller;
 
-//On détermine le nom du fichier contenant la définition du contrôleur
+// Détermine le nom du fichier contenant la définition du contrôleur
 $nom_fichier = 'Controllers/' . $nom_classe . '.php';
 
-//Si le fichier existe et est accessible en lecture
+// Si le fichier existe et est accessible en lecture
 if (is_readable($nom_fichier)) {
-    //On l'inclut et on instancie un objet de cette classe
+    // Inclut le fichier et instancie un objet de cette classe
     require_once $nom_fichier;
     new $nom_classe();
 } else {
