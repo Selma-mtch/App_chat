0f0e908fbe90ef88b1ec1272ee3d5a685ad1146c
Selma Mataiche <mataiche.selma77@gmail.com>
2025-01-13 14:08:17 +0100

    Create view_fin.php
    Création footer généralisé

---
+++ web/Php/Views/view_fin.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<script src=  "." $src1 "." ></script>";
+    echo "<script src=  "." $src2 "." ></script>";    
+    echo "</body>
+    <footer>
+        &copy; 2024 PingMe . Tous droits réservés.
+    </footer>
+  </html>" ;
+?>
