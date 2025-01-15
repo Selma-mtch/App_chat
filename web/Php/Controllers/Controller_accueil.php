73e78e0ab640bbe2678ea4c0e40c3c9a7e22575c
Selma Mataiche <mataiche.selma77@gmail.com>
2025-01-15 16:15:28 +0100

    Create Controller_accueil.php
    Creation controller accueil et fonction par défaut

---
+++ web/Php/Controllers/Controller_accueil.php
@@ -0,0 +1,26 @@
+<?php
+require_once "Models/Model.php"; // Inclusion du modèle
+require_once "Controller.php";
+
+class Controller_accueil extends Controller
+{
+
+    private $model ;
+
+    /**
+    * Constructeur
+    */
+    public function __construct()
+    {
+        $this->model =Model::getModel();  // Instancie le modèle
+        parent::__construct();
+        
+    }
+
+    /**
+     * Action par défaut du contrôleur
+     */
+    public function action_default(){
+        $this->render('accueil');
+    }
+}
