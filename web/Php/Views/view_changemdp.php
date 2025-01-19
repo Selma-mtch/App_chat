8a107d01e580fb12888df0b9429da45ae5f132e3
mehdisadki <98515764+mehdisadki@users.noreply.github.com>
2025-01-19 18:33:15 +0100

    Update view_changemdp.php

--- web/Php/Views/view_changemdp.php
+++ web/Php/Views/view_changemdp.php
@@ -6,32 +6,33 @@
     <title>Changer de mot de passe</title>
     <link rel="stylesheet" href="../css/changemdp.css">
 </head>
+<header>
+    <section id="header-container">
+        <a href="#" onclick="history.back(); return false;">
+            <img id="retour" src="../img/retour.webp" alt="Retour">
+        </a>
+        <h1 id="titre">
+            Changer de mot de passe
+        </h1>
+    </section>
+</header>
 <body>
-    <header>
-        <section id="header-container">
-            <a href="#" onclick="history.back(); return false;">
-                <img id="retour" src="../img/retour.webp" alt="Retour">
-            </a>
-            <h1 id="titre">Changer de mot de passe</h1>
-        </section>
-    </header>
-    
-    <main class="form-container">
+    <div class="container">
         <div class="form-wrapper">
-            <h1>Changer votre mot de passe</h1>
-            <form action="#" method="POST">
+            <i class="fas fa-lock fa-3x"></i>
+            <h2>Entrer votre mot de passe actuel et nouveau</h2>
+            <form method='POST' action="index.php?controller=changemdp&action=changemdp">
                 <input type="password" name="current-password" placeholder="Mot de passe actuel" required>
                 <input type="password" name="new-password" placeholder="Nouveau mot de passe" required>
-                <input type="password" name="confirm-password" placeholder="Confirmer le nouveau mot de passe" required>
-                <button type="submit">Enregistrer</button>
+                <input type="password" name="confirm-password" placeholder="Confirmer le mot de passe" required>
+                <button type="submit"><i class="fas fa-save"></i> Enregistrer</button>
             </form>
         </div>
-        <?php if (isset($message) && !empty($message)): ?>
-        <div class="message">
-            <?php echo htmlspecialchars($message); ?>
-        </div>
-        <?php endif ?>
-    </main>
+    </div>
+    <?php if (isset($message) && !empty($message)): ?>
+    <div class="message"><?= htmlspecialchars($message) ?></div>
+    <?php endif; ?>
+
 </body>
 </html>
 
@@ -39,6 +40,4 @@
 $src1 = "";
 $src2 = "";
 require 'view_fin.php';
-
 ?>
- 
